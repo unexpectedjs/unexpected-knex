53f7e4c7898ca6b5d0d3230e5e6fd425ca95f201
Joel Mukuthu <jmu@one.com>
2016-09-22 17:14:09 +0200

    Refacor assertions to eliminate the need for a factory function

--- test/unexpected-knex.spec.js
+++ test/unexpected-knex.spec.js
@@ -1,100 +1,48 @@
-const sqlite3 = require('sqlite3');
-const knexModule = require('knex');
+const knexFactory = require('knex');
 const unexpected = require('unexpected');
 const unexpectedFs = require('unexpected-fs');
 const unexpectedRequire = require('unexpected-require');
-const unexpectedKnexFactory = require('../lib/unexpected-knex');
+const unexpectedKnex = require('../lib/unexpected-knex');
+
+const expect = unexpected.clone()
+    .use(unexpectedFs)
+    .use(unexpectedRequire)
+    .use(unexpectedKnex);
 
 describe('unexpected-knex', function () {
-    describe('on initialization', function () {
-        const expect = unexpected.clone();
-
-        it('throws if not provided a knex instance', function () {
-            return expect(
-                function () {
-                    unexpectedKnexFactory();
+    describe('<knex> to apply migration <migration>', function () {
+        it('applies a migration', function () {
+            const knex = knexFactory({
+                client: 'sqlite3',
+                connection: {
+                    filename: ':memory:'
                 },
-                'to error with',
-                'No knex instance provided'
-            );
-        });
-
-        it('throws if not provided a valid knex instance', function () {
-            return expect(
-                function () {
-                    unexpectedKnexFactory(function () {});
-                },
-                'to error with',
-                'Invalid knex instance provided'
-            );
-        });
-    });
-
-    describe('after initialization', function () {
-        const db = new sqlite3.Database(':memory:');
-        const knex = knexModule({
-            client: 'sqlite3',
-            connection: {
-                filename: ':memory:'
-            },
-            migrations: {
-                directory: '/path/to/migrations'
-            }
-        });
-        const unexpectedKnex = unexpectedKnexFactory(knex);
-        const expect = unexpected.clone()
-            .use(unexpectedFs)
-            .use(unexpectedRequire)
-            .use(unexpectedKnex)
-            .addType({
-                name: 'unexpectedKnex',
-                base: 'object',
-                identify: function (value) {
-                    return value &&
-                        value.name === 'unexpected-knex' &&
-                        typeof value.installInto === 'function';
-                },
-                inspect: function (value, depth, output, inspect) {
-                    output.appendInspected('unexpected-knex');
+                migrations: {
+                    directory: '/path/to/migrations'
                 }
-            })
-            .addAssertion(
-                '<unexpectedKnex> to apply migration <migration>',
-                function (expect, unexpectedKnex, migration) {
-                    expect.errorMode = 'nested';
-                    const filename = migration.name;
-                    return expect(
-                        migration,
-                        'with fs mocked out', {
-                            '/path/to/migrations': {
-                                [filename]: ''
-                            }
-                        },
-                        'with require mocked out', {
-                            [`/path/to/migrations/${filename}`]: migration
-                        },
-                        'to apply'
-                    );
-                }
-            );
-
-        after(function () {
-            db.close();
-        });
-
-        describe('<migration> to apply', function () {
-            it('applies a migration', function () {
-                const migration = {
-                    up: knex => knex.schema.createTable('foo', table => {
-                        table.timestamps();
-                    }),
-                    down: knex => knex.schema.dropTable('foo'),
-                    name: '1-foo.js'
-                };
-                return expect(unexpectedKnex, 'to apply migration', migration)
-                    .then(() => expect(knex, 'to have table', 'foo'))
-                    .then(() => knex.schema.dropTable('foo'));
             });
+
+            const migration = {
+                up: knex => knex.schema.createTable('foo', table => {
+                    table.timestamps();
+                }),
+                down: knex => knex.schema.dropTable('foo'),
+                name: '1-foo.js'
+            };
+
+            return expect(knex,
+                'with fs mocked out', {
+                    '/path/to/migrations': {
+                        '1-foo.js': ''
+                    }
+                },
+                'with require mocked out', {
+                    '/path/to/migrations/1-foo.js': migration
+                },
+                'to apply migration', migration
+            )
+            .then(() => expect(knex, 'to have table', 'foo'))
+            .then(() => knex.schema.dropTable('foo'));
         });
     });
 });
