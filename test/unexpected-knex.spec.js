c08926caf0f724e4af43546b3ae343229f22fa3f
Joel Mukuthu <jmu@one.com>
2016-09-27 10:51:52 +0200

    Add more tests
    Add tests for:
    - <knex> not to have table <string>
    - <knex> to have column <object>
    - <knex> not to have column <object>
    - <knex> to have columns <object>
    - <knex> not to have columns <object>

--- test/unexpected-knex.spec.js
+++ test/unexpected-knex.spec.js
@@ -55,6 +55,433 @@ describe('unexpected-knex', function () {
         });
     });
 
+    describe('<knex> not to have table <string>', function () {
+        it('fulfils if the table does not exist', function () {
+            return expect(
+                expect(knex, 'not to have table', 'foo'),
+                'to be fulfilled'
+            );
+        });
+
+        it('rejects if the table exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.timestamps();
+            })
+            .then(() => expect(
+                expect(knex, 'not to have table', 'foo'),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have table 'foo'`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+    });
+
+    describe('<knex> to have column <object>', function () {
+        it('fulfils if the column exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'to have column', { foo: 'bar' }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the column does not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'to have column', { foo: 'baz' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have column { foo: 'baz' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the table itself does not exist', function () {
+            return expect(
+                expect(knex, 'to have column', { foo: 'bar' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have column { foo: 'bar' }`
+            );
+        });
+
+        it('throws if the object has more than one key', function () {
+            return expect(
+                () => expect(knex, 'to have column', { foo: 'bar', bar: 'baz' }),
+                'to error with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have column { foo: 'bar', bar: 'baz' }
+                  Provide a single column in the form: { tableName: columnName }`
+            );
+        });
+    });
+
+    describe('<knex> not to have column <object>', function () {
+        it('fulfils if the column does not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have column', { foo: 'baz' }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the column exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have column', { foo: 'bar' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('fulfils if the table itself does not exist', function () {
+            return expect(
+                expect(knex, 'not to have column', { foo: 'bar' }),
+                'to be fulfilled'
+            );
+        });
+
+        it('throws if the object has more than one key', function () {
+            return expect(
+                () => expect(knex, 'not to have column', { foo: 'bar', bar: 'baz' }),
+                'to error with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have column { foo: 'bar', bar: 'baz' }
+                  Provide a single column in the form: { tableName: columnName }`
+            );
+        });
+    });
+
+    describe('<knex> not to have column <object>', function () {
+        it('fulfils if the column does not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have column', { foo: 'baz' }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the column exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have column', { foo: 'bar' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('fulfils if the table itself does not exist', function () {
+            return expect(
+                expect(knex, 'not to have column', { foo: 'bar' }),
+                'to be fulfilled'
+            );
+        });
+
+        it('throws if the object has more than one key', function () {
+            return expect(
+                () => expect(knex, 'not to have column', { foo: 'bar', bar: 'baz' }),
+                'to error with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have column { foo: 'bar', bar: 'baz' }
+                  Provide a single column in the form: { tableName: columnName }`
+            );
+        });
+    });
+
+    describe('<knex> to have columns <object>', function () {
+        it('fulfils if all the columns exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+                table.string('baz');
+            })
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: [ 'bar', 'baz' ] }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if any of the columns does not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: [ 'bar', 'baz' ] }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: [ 'bar', 'baz' ] }
+                  expected knex to have column { foo: 'baz' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if all columns do not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: [ 'baz', 'quux' ] }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: [ 'baz', 'quux' ] }
+                  expected knex to have column { foo: 'baz' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the table itself does not exist', function () {
+            return expect(
+                expect(knex, 'to have columns', { foo: [ 'baz', 'quux' ] }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: [ 'baz', 'quux' ] }
+                  expected knex to have column { foo: 'baz' }`
+            );
+        });
+
+        it('fulfils if all the columns in all the tables exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+                table.string('baz');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: [ 'bar', 'baz' ], bar: 'baz' }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('rejects if any of the columns in any of the tables does not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: [ 'bar', 'baz' ], bar: 'baz' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: [ 'bar', 'baz' ], bar: 'baz' }
+                  expected knex to have column { foo: 'baz' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('rejects if all columns in all of the tables do not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: 'quux', bar: 'quux' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: 'quux', bar: 'quux' }
+                  expected knex to have column { foo: 'quux' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('rejects if one of the tables do not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'to have columns', { foo: 'bar', bar: 'quux' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: 'bar', bar: 'quux' }
+                  expected knex to have column { bar: 'quux' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if the tables themselves do not exist', function () {
+            return expect(
+                expect(knex, 'to have columns', { foo: 'bar', bar: 'quux' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                to have columns { foo: 'bar', bar: 'quux' }
+                  expected knex to have column { foo: 'bar' }`
+            );
+        });
+    });
+
+    describe('<knex> not to have columns <object>', function () {
+        it('fulfils if all the columns do not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+                table.string('baz');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: [ 'quux1', 'quux2' ] }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if any of the columns exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: [ 'bar', 'baz' ] }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have columns { foo: [ 'bar', 'baz' ] }
+                  expected knex not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('rejects if all columns exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+                table.string('baz');
+            })
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: [ 'bar', 'baz' ] }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have columns { foo: [ 'bar', 'baz' ] }
+                  expected knex not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'));
+        });
+
+        it('fulfils if the table itself does not exist', function () {
+            return expect(
+                expect(knex, 'not to have columns', { foo: [ 'baz', 'quux' ] }),
+                'to be fulfilled'
+            );
+        });
+
+        it('fulfils if all the columns in all the tables do not exist', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: [ 'quux1', 'quux2' ], bar: 'quux1' }),
+                'to be fulfilled'
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('rejects if any of the columns in any of the tables exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: [ 'baz', 'bar' ], bar: 'quux' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have columns { foo: [ 'baz', 'bar' ], bar: 'quux' }
+                  expected knex not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('rejects if all columns in all of the tables exists', function () {
+            return knex.schema.createTable('foo', table => {
+                table.string('bar');
+            })
+            .then(() => knex.schema.createTable('bar', table => {
+                table.string('baz');
+            }))
+            .then(() => expect(
+                expect(knex, 'not to have columns', { foo: 'bar', bar: 'baz' }),
+                'to be rejected with',
+                dontIndent`
+                expected
+                ${knexOutputBlock}
+                not to have columns { foo: 'bar', bar: 'baz' }
+                  expected knex not to have column { foo: 'bar' }`
+            ))
+            .then(() => knex.schema.dropTable('foo'))
+            .then(() => knex.schema.dropTable('bar'));
+        });
+
+        it('fulfils if the tables themselves do not exist', function () {
+            return expect(
+                expect(knex, 'not to have columns', { foo: 'bar', bar: 'quux' }),
+                'to be fulfilled'
+            );
+        });
+    });
+
     describe('<knex> to apply migration <string>', function () {
         it('applies a migration', function () {
             return expect(knex,
